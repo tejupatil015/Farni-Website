5088bec673efe396e2bcab66f40add20267b4c8f
Tejupatil015 <tejupatil1506@gmail.com>
2026-01-27 20:39:34 +0530

    Scrollbar adding events

--- logic.js
+++ logic.js
@@ -25,3 +25,25 @@ const scrollRevealOption = {
   origin: "bottom",
   duration: 1000,
 };
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".header__content div", {
+  duration: 1000,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".deals__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
